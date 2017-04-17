4c670f9dc445e6129bb6b8edd14be0c042b2a6e7
Jared Bagley <cabrill@gmail.com>
2017-04-17 22:39:12 +0300

    Tournament status is now rendered on screen.

---
+++ FishKing/FishKing/FishKing/GumRuntimes/TournamentStatusRuntime.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishKing.GumRuntimes
+{
+    partial class TournamentStatusRuntime
+    {
+        partial void CustomInitialize()
+        {
+            
+        }
+
+        public void CustomActivity()
+        {
+            if (!Wave1AnimateAnimation.IsPlaying())
+            {
+                Wave1AnimateAnimation.Play();
+            }
+            if (!Wave2AnimateAnimation.IsPlaying())
+            {
+                Wave2AnimateAnimation.Play();
+            }
+            if (!Wave3AnimateAnimation.IsPlaying())
+            {
+                Wave3AnimateAnimation.Play();
+            }
+            if (!Wave4AnimateAnimation.IsPlaying())
+            {
+                Wave4AnimateAnimation.Play();
+            }
+            if (!TournamentFishInstance.FishSwimAnimation.IsPlaying())
+            {
+                TournamentFishInstance.FishSwimAnimation.Play();
+            }
+            if (!TournamentFishInstance1.FishSwimAnimation.IsPlaying())
+            {
+                TournamentFishInstance1.FishSwimAnimation.Play();
+            }
+            if (!TournamentFishInstance2.FishSwimAnimation.IsPlaying())
+            {
+                TournamentFishInstance2.FishSwimAnimation.Play();
+            }
+        }
+
+        private void CustomDestroy()
+        {
+
+
+        }
+
+        private static void CustomLoadStaticContent(string contentManagerName)
+        {
+
+
+        }
+    }
+}
--- FishKing/FishKing/FishKing/Screens/GameScreen.cs
+++ FishKing/FishKing/FishKing/Screens/GameScreen.cs
@@ -155,7 +155,7 @@ namespace FishKing.Screens
             {
                 AmbientAudioManager.UpdateAmbientSoundSources();
             }
-
+            TournamentStatusRuntimeInstance.CustomActivity();
         }
 
         private void UpdateCamera()
